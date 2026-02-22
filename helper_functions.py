df797856f5534c16a4faaad6c8068dde3aceb419
adi-san <adityasan@gmail.com>
2026-02-22 13:26:22 -0600

    67 lol 2.0

--- helper_functions.py
+++ helper_functions.py
@@ -1,1 +1,17 @@
-print(9021067)
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+def relu(x):
+    return np.maximum(0, x)
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
+def mse(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+def mse_derivative(y_true, y_pred):
+    print("y_true:")
+
+print(1)
+
